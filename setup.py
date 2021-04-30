87883b7b0f92fdd1a9b3b5b6b690141362aacdf4
Gustavo Almeida <51726412+GusAlmeida93@users.noreply.github.com>
2021-04-29 23:03:46 -0300

    Removed streamlit and app structure, this project will be for EDA

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='networkanalysis',
+      version='1.0.0',
+      description='Python Distribution Utilities',
+      author='Gustavo Almeida',
+      author_email='gusalmeida93@ggmail.com',
+      url='https://github.com/GusAlmeida93/network-analysis',
+      packages=['pandas', 'sklearn', 'pyvis', 'jupyter', 'networkx', 'matplotlib'],
+     )
